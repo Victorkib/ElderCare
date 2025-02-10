fed578aec56f78e7d14d9f7c22910c8d4bcdcc0f
Victorkib01~ <victor3720kibiwott@gmail.com>
2025-02-10 15:41:42 +0300

    reordered

--- client/src/pages/LoggedInLanding/LoggedInLandingPage.jsx
+++ client/src/pages/LoggedInLanding/LoggedInLandingPage.jsx
@@ -109,17 +109,6 @@ const LoggedInLandingPage = () => {
         </button>
         <button
           className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow text-center"
-          onClick={() => navigate('/set-reminder')}
-        >
-          <h2 className="text-xl font-semibold text-gray-800 mb-2">
-            Set Reminder
-          </h2>
-          <p className="text-gray-600">
-            Create a new task or medication reminder.
-          </p>
-        </button>
-        <button
-          className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow text-center"
           onClick={() => navigate('/caregiver')}
         >
           <h2 className="text-xl font-semibold text-gray-800 mb-2">
@@ -127,6 +116,17 @@ const LoggedInLandingPage = () => {
           </h2>
           <p className="text-gray-600">
             Update the Elderly Care Management team.
+          </p>
+        </button>
+        <button
+          className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow text-center"
+          onClick={() => navigate('/scheduler')}
+        >
+          <h2 className="text-xl font-semibold text-gray-800 mb-2">
+            Set Reminder
+          </h2>
+          <p className="text-gray-600">
+            Create a new task or medication reminder.
           </p>
         </button>
       </div>
