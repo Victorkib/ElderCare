ea2ce87d6f4f900ad04d4b0dccf084093ec3c606
Victorkib01~ <victor3720kibiwott@gmail.com>
2025-02-10 14:49:15 +0300

    apiUrlChanged

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const devUrl = 'http://localhost:5500/api';
+// const devUrl = 'http://localhost:5500/api';
+const devUrl = 'https://eldercarebackend.onrender.com/api';
 
 const apiRequest = axios.create({
   baseURL: devUrl,
